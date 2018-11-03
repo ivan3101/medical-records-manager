18ee2475ae9b2192407163a944e03f51912b9d59
Ivan De Menezes <ivandmenezes@gmail.com>
2018-11-03 18:15:56 -0400

    Database connected

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
 import { createServer } from "http";
 import { variables } from "./config/globals";
 import { Server } from "./server";
+import {DbConnService, IDbConnParams} from "./services/dbConnService";
 
 const app = new Server().App;
-const server = createServer(app);
-
 const port = variables.port;
+const server = createServer(app);
 
 server.listen(port);
 
-server.on("listening", () => {
-    // console.log(`Server is listening on port ${port}`);
+server.on("listening", async () => {
+    const dbParams: IDbConnParams = {
+        dbHost: variables.db_host,
+        dbName: variables.db_name,
+        dbPassword: variables.db_password,
+        dbPort: variables.db_port,
+        dbUsername: variables.db_username
+    };
+    try {
+        const dbConnection = await new DbConnService(dbParams).DbConnection;
+    } catch (e) {
+        console.error("No se pudo establecer conexión con MongoDB", e);
+    }
 });
 
 server.on("close", () => {
